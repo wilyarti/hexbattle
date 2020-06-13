38a22503d406d9bf43fa1b6e6b73fc31d4240987
wilyarti <wilyarti@gmail.com>
2020-06-14 01:01:18 +1000

    Add CSS transitions, animations and assets.

--- js/game.js
+++ js/game.js
@@ -1,19 +1,26 @@
-let game = {
+var animations = [];
+
+var game = {
   score: 0,
   startTime: new Date(),
   speed: 1,
   started: false,
   updated: new Date(),
-  gameOver: false
+  gameOver: false,
 }
-let level = {
+var level = {
   maxNumber: 16,
-  advanceRate: 2500, // ms per square (*5 = total)
   damage: 5,
-  spawnTime: 15000,
-  levelNumber: 1
+  timeOffset: 8000,
+  levelNumber: 1,
+  DURATION: 10000,
+  scoreElement: document.getElementById('score'),
+  misses: 0,
+  rate: 1,
+  RATE_INTERVAL: .05 //playbackRate will increase by .05 for each letter... so after 20 letters, the rate of falling will be 2x what it was at the start
+
 }
-let enemies = [];
+var enemies = [];
 
 let shooter = [0, 0, 0, 0, 0, 0, 0, 0];
 const container = document.getElementById("container");
@@ -22,18 +29,38 @@ const container = document.getElementById("container");
 function resetGame() {
   document.getElementById("gameOver").classList.add("hidden-content");
   document.getElementById("startGame").classList.add("hidden-content");
-  game.started = true;
-  game.startTime = new Date();
+
   resetShooter()
   enemies = [];
+  clearInterval(cleanupInterval);
+  getAllAnimations().forEach(function(anim) {
+    anim.pause();
+  });
+  // remove all animations
+  container.querySelectorAll('.enemy-animated').forEach(function (enemy) {
+    enemy.parentNode.removeChild(enemy);
+  });
+  game = {
+    score: 0,
+    startTime: new Date(),
+    speed: 1,
+    started: false,
+    updated: new Date(),
+    gameOver: false,
+  }
   level = {
     maxNumber: 16,
-    advanceRate: 2500, // ms per square (*5 = total)
     damage: 5,
-    spawnTime: 15000,
-    levelNumber: 1
+    timeOffset: 8000,
+    levelNumber: 1,
+    DURATION: 10000,
+    scoreElement: document.getElementById('score'),
+    misses: 0,
+    rate: 1,
+    RATE_INTERVAL: .05 //playbackRate will increase by .05 for each letter... so after 20 letters, the rate of falling will be 2x what it was at the start
   }
 }
+
 function start() {
   if (game.started) {
     console.log("Warning: game already started.");
@@ -56,38 +83,7 @@ function main(elementId) {
   }
   let currentTime = new Date();
   let timeDiff = currentTime - game.updated; //in ms
-  // move/delete units
-  enemies = enemies.filter(function (enemy) {
-    if (typeof enemy === "undefined") {
-      // delete
-      console.log("Blank enemy. Deleting.")
-      return
-    }
-    if (currentTime - enemy.updated > level.advanceRate) {
-      if (enemy.locationY === 6) {
-        game.score -= level.damage;
-        console.log("Game over.");
-        container.removeChild(enemy.cell);
-        // delete
-        return
-      } else {
-        enemy.locationY++;
-        console.log("Enemy to be moved");
-        enemy.cell.style.gridColumnStart = enemy.locationX;
-        enemy.cell.style.gridRowStart = enemy.locationY;
-        let text = document.createElement("div");
-        text.classList.add("enemy");
-        text.innerText = enemy.value;
-        enemy.cell.append(text);
-        enemy.cell.append(enemy.bomb);
-        container.appendChild(enemy.cell);
-        enemy.updated = new Date();
-        // update and move
-      }
-    }
-    // loop or return updated
-    return enemy;
-  });
+
   // update score
   if (game.score < 0) {
     game.gameOver = true;
@@ -95,7 +91,7 @@ function main(elementId) {
   }
   calculateScore();
   // spawn units
-  if (timeDiff > level.spawnTime) {
+  if (timeDiff > level.timeOffset) {
     spawn();
   }
   if (enemies.length === 0) {
@@ -126,13 +122,16 @@ function shoot(column) {
   console.log(binaryNumber);
   calculateShooter();
   // Check if matches an enemy
-
   enemies = enemies.map(enemy => {
     if (typeof enemy === "undefined") {
       return
     }
     if ((parseInt(enemy.value, 16) === parseInt(binaryNumber, 2))) {
-      container.removeChild(enemy.cell);
+      let ship = document.getElementById(enemy.value);
+      ship.parentNode.removeChild(ship);
+      enemy.bomb.classList.add("explosion");
+      animations.find(x => x.code === enemy.value).animation.pause(); // pause animation
+      setTimeout(() => { container.removeChild(enemy.bomb)}, 1000);
       game.score += 10;
     } else {
       return enemy;
@@ -151,20 +150,56 @@ function shoot(column) {
 
 function spawn() {
   console.log("Spawning enemy.");
-  // create new enemy
-  let bomb = document.createElement("img")
-  bomb.src = "img/bomb.svg"
-  bomb.classList.add("grid-img")
+  let value = getRandomInt(0, level.maxNumber).toString(16);
+
+  let idx = Math.floor(Math.random() * 255);
+  let x = (Math.random() * 75) + 'vw';
+  let bomb = document.createElement('div');
+  //let ship = document.createElement('img');
+  let letter = document.createElement('span');
+  let letterText = document.createElement('b');
+  letterText.textContent = value;
+  letterText.classList.add("glow");
+  letter.classList.add("enemy-animated");
+
+  letter.id = value;
+  //ship.src = "img/Ship_01.png";
+  //letter.appendChild(ship);
+  letter.appendChild(letterText);
+  bomb.appendChild(letter);
+  container.appendChild(bomb);
+  console.log(bomb);
+  let animation = bomb.animate([
+    {transform: 'translate3d(' + x + ',-2.5vh,0)'},
+    {transform: 'translate3d(' + x + ',82.5vh,0)'}
+  ], {
+    duration: level.DURATION,
+    easing: 'linear',
+    fill: 'both'
+  });
+
+  animations.push({animation: animation, element: bomb, code: value});
+  level.rate = level.rate + level.RATE_INTERVAL;
+  animation.playbackRate = level.rate;
+
+  //If an animation finishes, we will consider that as a miss, so we will remove it from the active animations array and increment our miss count
+  animation.onfinish = function(e) {
+    let target = bomb;
+    target.classList.add('missed');
+    handleMisses();
+  }
+// create new enemy
   enemies.push({
-    value: getRandomInt(0, level.maxNumber).toString(16),
+    value: value,
     locationX: getRandomInt(1, 7),
     locationY: 1,
     speed: 0,
     alive: true,
     updated: new Date(),
-    cell: document.createElement("div"),
     bomb: bomb
   })
+  console.log(enemies);
+
   // Reset game timer
   game.updated = new Date();
 }
@@ -174,13 +209,10 @@ function levelUp() {
   // maxNumber: 16,
   //   advanceRate: 2500, // ms per square (*5 = total)
   //   damage: 5,
-  //   spawnTime: 15000,
+  //   timeOffset: 15000,
   //   levelNumber: 1
-  level.maxNumber *=2;
-  level.advanceRate *= 0.80;
-  level.damage *= 2;
-  level.spawnTime *= 0.80;
   level.levelNumber++;
+  level.maxNumber *= 2;
   // Increase difficulty parameters
   calculateLevel();
 
@@ -207,9 +239,46 @@ function resetShooter() {
   }
   calculateShooter();
 }
+
 function calculateScore() {
   document.getElementById("score").innerText = game.score;
 }
+
 function calculateLevel() {
   document.getElementById("level").innerText = level.levelNumber;
+}
+
+
+//Periodically remove missed elements, and lower the interval between falling elements
+var cleanupInterval = setInterval(function () {
+  level.timeOffset = level.timeOffset * 4 / 5;
+  cleanup();
+}, 20000);
+
+function cleanup() {
+  container.querySelectorAll('.missed').forEach(function (missed) {
+    container.removeChild(missed);
+  });
+}
+
+//Firefox 48 supports document.getAnimations as per latest spec, Chrome 52 and polyfill use older spec
+function getAllAnimations() {
+  if (document.getAnimations) {
+    return document.getAnimations();
+  } else if (document.timeline && document.timeline.getAnimations) {
+    return document.timeline.getAnimations();
+  }
+  return [];
+}
+
+//When a miss is registered, check if we have reached the max number of misses
+function handleMisses() {
+  level.misses++;
+  game.score = game.score - (1 * level.damage);
+  let missedMarker = document.querySelector('.misses:not(.missed)');
+  if (missedMarker) {
+    missedMarker.classList.add('missed');
+  } else {
+    //game.gameOver = true;
+  }
 }
